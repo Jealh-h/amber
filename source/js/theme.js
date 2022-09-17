c230abe1c0fbf1c7cf2e7b965a13b311eb3a0f2a
huhengdong <huhengdong@bytedance.com>
2022-09-17 18:04:11 +0800

    fix: img tag

--- source/js/theme.js
+++ source/js/theme.js
@@ -7,7 +7,7 @@
 
   document.querySelector('html').setAttribute('data-theme', dataTheme);
   themeToggleBtn.innerHTML = `
-    <img src="${CONFIG.root}images/${theme}.svg"' />
+    <img src="${CONFIG.root}images/${theme}.svg" />
   `;
 
   themeToggleBtn.addEventListener('click', () => {
