8a5f384dd9f0b0c7458bc221d21d5c7fd1e6d8fc
huhengdong <huhengdong@bytedance.com>
2022-09-17 15:22:57 +0800

    feat: theme toggle

--- scripts/filters/default-injects.js
+++ scripts/filters/default-injects.js
@@ -3,22 +3,30 @@
 'use strict';
 
 const points = require('../events/lib/injects-point');
+const js = hexo.extend.helper.get('js').bind(hexo);
 
-hexo.extend.filter.register('theme_inject', injects => {
-  let filePath = hexo.theme.config.custom_file_path;
+hexo.extend.filter.register(
+  'theme_inject',
+  (injects) => {
+    const filePath = hexo.theme.config.custom_file_path;
 
-  if (!filePath) return;
+    if (!filePath) return;
 
-  points.views.forEach(key => {
-    if (filePath[key]) {
-      injects[key].file('custom', filePath[key]);
-    }
-  });
+    points.views.forEach((key) => {
+      if (filePath[key]) {
+        injects[key].file('custom', filePath[key]);
+      }
+    });
 
-  points.styles.forEach(key => {
-    if (filePath[key]) {
-      injects[key].push(filePath[key]);
-    }
-  });
+    points.styles.forEach((key) => {
+      if (filePath[key]) {
+        injects[key].push(filePath[key]);
+      }
+    });
+  },
+  99
+);
 
-}, 99);
+hexo.extend.injector.register('body_end', () => {
+  return js('./theme.js');
+});
